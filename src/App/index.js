4a81d6e2991b6cf7f9aa67faa18521935aa67777
fabilqis <fadillah.bilqis@gmail.com>
2018-09-10 23:55:13 +0700

    Change font  size

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Headers from '../Component/Headers'
-import { Grid } from 'semantic-ui-react'
+import { Grid, List } from 'semantic-ui-react'
 import images from '../Component/Headers/eng.png'
 
 const styles = {
@@ -12,6 +12,9 @@ const styles = {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)'
+    },
+    footers : {
+        fontSize : "1.2em"
     }
 }
 
@@ -21,15 +24,22 @@ class App extends React.Component {
         <div style={styles.position}>
             <div style={styles.child}>
                 <Grid centered>
-                <Grid.Row>
-                    <Grid.Column mobile={16} tablet={8} computer={4}>
-                        <img src={images} alt="imgs"/>
-                    </Grid.Column>
+                    <Grid.Row>
+                        <Grid.Column mobile={16} tablet={8} computer={4}>
+                            <img src={images} alt="imgs"/>
+                        </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row>
+                        <Grid.Column mobile={16} tablet={8} computer={4}>
+                            <Headers/>
+                        </Grid.Column>
                     </Grid.Row>
                     <Grid.Row>
-                    <Grid.Column mobile={16} tablet={8} computer={4}>
-                        <Headers/>
-                    </Grid.Column>
+                    <Grid  reversed='mobile' columns='equal' style={styles.footers}>
+                            <Grid.Column><a className="text-dark" href="https://www.linkedin.com/in/fadillahbilqis/" target="_blank" rel="noopener noreferrer">LinkedIn</a></Grid.Column>
+                            <Grid.Column><a className="text-dark" href="https://github.com/fabilqis" target="_blank" rel="noopener noreferrer">Github</a></Grid.Column>
+                            <Grid.Column><a className="text-dark" href="https://www.behance.net/jo_anne" target="_blank" rel="noopener noreferrer">Behance</a> </Grid.Column>
+                    </Grid>
                     </Grid.Row>
                 </Grid>
             </div>
--- src/Component/Headers/index.js
+++ src/Component/Headers/index.js
@@ -5,16 +5,12 @@ import { Container, List } from 'semantic-ui-react';
 const styles = {
    
     bodies : {
-        fontSize : "3em",
+        fontSize : "3.2em",
         fontFamily: 'Lalezar',
         textAlign: 'center'
     },
-    footers : {
-        fontSize : "4em",
-        textAlign: 'center'
-    },
     foots : {
-        fontSize : '1.2em',
+        fontSize : '1.4em',
         textAlign: 'center'
     }
 }
@@ -29,16 +25,9 @@ class Headers extends React.Component {
                 </div>
                 <div>
                     <div style={styles.foots}>
-                        <span>Email : fadillah@merahlabs.id</span>
+                        <p>Email : fadillah@merahlabs.id</p>
                     </div>  
-                </div>
-                <div style={styles.footers}>
-                <List horizontal >
-                    <List.Item><a className="text-dark" href="https://www.linkedin.com/in/fadillahbilqis/" target="_blank" rel="noopener noreferrer">LinkedIn</a></List.Item>
-                    <List.Item><a className="text-dark" href="https://github.com/fabilqis" target="_blank" rel="noopener noreferrer">Github</a></List.Item>
-                    <List.Item><a className="text-dark" href="https://www.behance.net/jo_anne" target="_blank" rel="noopener noreferrer">Behance</a></List.Item>
-                </List>
-                </div>
+                </div> 
             </Container>
         </div>
         )
